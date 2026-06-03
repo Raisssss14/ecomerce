b73c1634c46a1be02676eac2715db730ac991367
Raisssss14 <raizkaz@gmail.com>
2026-06-04 04:59:41 +0700

    Refactor server.js to improve structure and readability

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
-const express = require("express");
-const mysql = require("mysql2");
+const express = require("express");b
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const midtransClient = require("midtrans-client");
@@ -13,11 +12,15 @@ app.use(bodyParser.json());
 
 /* MYSQL */
 
+const mysql = require("mysql2");
+require("dotenv").config();
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err)=>{
